0dfe5645e1767184b7ce94e4e59a419681d28192
Ina Yoon <yia0319@gmail.com>
2023-12-30 00:12:15 -0500

    backend static method for the Image

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+//절대 경로를 사용하기 위해서 path 를 씀
+// ../upload로 하는 이유는 ../이 없을 경우 src폴더안에 uploads를 찾게됨
+app.use(express.static(path.join(__dirname, "../uploads")));
+
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
