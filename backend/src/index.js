359e4a6540071bcedaa97f5d0a2c1520b0f7e139
Ina Yoon <yia0319@gmail.com>
2024-01-01 12:44:07 +0900

    Created User schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors");
 const port = 4000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongoose connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 //절대 경로를 사용하기 위해서 path 를 씀
 // ../upload로 하는 이유는 ../이 없을 경우 src폴더안에 uploads를 찾게됨
