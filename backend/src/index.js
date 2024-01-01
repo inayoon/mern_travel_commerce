c9d17649cd9cbc4a5f34ff256fa6c366adac3be6
Ina Yoon <yia0319@gmail.com>
2024-01-01 16:40:49 +0900

    Created signup form with react-hook-form

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,14 +19,17 @@ mongoose
     console.error(err);
   });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", (req, res, next) => {
+  setImmediate(() => {
+    next(new Error("it is an error!"));
+  });
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+app.use((error, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(error.message || "server error");
 });
+
 //절대 경로를 사용하기 위해서 path 를 씀
 // ../upload로 하는 이유는 ../이 없을 경우 src폴더안에 uploads를 찾게됨
 app.use(express.static(path.join(__dirname, "../uploads")));
--- frontend/src/pages/RegisterPage/index.jsx
+++ frontend/src/pages/RegisterPage/index.jsx
@@ -1,5 +1,114 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function RegisterPage() {
-  return <div>index</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = ({ email, password, name }) => {
+    reset();
+  };
+
+  const userEmail = {
+    required: "This field is required",
+  };
+
+  const userName = {
+    required: "This field is required",
+  };
+
+  const userPassword = {
+    required: "This field is required",
+    minLength: {
+      value: 6,
+      message: "Password must be at least 6 characters",
+    },
+  };
+
+  return (
+    <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
+      <div className="p-6 bg-white rounded-md shadow-md">
+        <h1 className="text-3xl font-bold text-center">Sing Up</h1>
+        <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-2">
+            <label
+              htmlFor="email"
+              className="text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("email", userEmail)}
+            />
+            {errors?.email && (
+              <div>
+                <span className="text-red-500">{errors.email.message}</span>
+              </div>
+            )}
+          </div>
+
+          <div className="mb-2">
+            <label
+              htmlFor="name"
+              className="text-sm font-semibold text-gray-800"
+            >
+              Name
+            </label>
+            <input
+              type="name"
+              id="name"
+              className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("name", userName)}
+            />
+            {errors?.name && (
+              <div>
+                <span className="text-red-500">{errors.name.message}</span>
+              </div>
+            )}
+          </div>
+
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("password", userPassword)}
+            />
+            {errors?.password && (
+              <div>
+                <span className="text-red-500">{errors.password.message}</span>
+              </div>
+            )}
+          </div>
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full bg-black text-white px-4 py-2 rounded-md hover:bg-gray-700 duration-200"
+            >
+              Sign-Up
+            </button>
+          </div>
+          <p className="mt-8 text-sm text-center text-gray-700">
+            Do you have an account?{" "}
+            <a href="/login" className="font-medium hover:underline ">
+              Log-In
+            </a>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 }
