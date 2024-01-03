7c69d64688673cf1de543fe17f03368393e67acb
Ina Yoon <yia0319@gmail.com>
2024-01-03 23:30:43 +0900

    Create signin function

--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -1,5 +1,99 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../store/thunkFunctions.js";
 
 export default function LoginPage() {
-  return <div>index</div>;
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = ({ email, password }) => {
+    const body = {
+      email,
+      password,
+    };
+    dispatch(loginUser(body));
+    reset();
+    // navigate('/login');
+  };
+
+  const userEmail = {
+    required: "This field is required",
+  };
+
+  const userPassword = {
+    required: "This field is required",
+    minLength: {
+      value: 6,
+      message: "Password must be at least 6 characters",
+    },
+  };
+
+  return (
+    <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
+      <div className="p-6 bg-white rounded-md shadow-md">
+        <h1 className="text-3xl font-bold text-center">Sign In</h1>
+        <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-2">
+            <label
+              htmlFor="email"
+              className="text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("email", userEmail)}
+            />
+            {errors?.email && (
+              <div>
+                <span className="text-red-500">{errors.email.message}</span>
+              </div>
+            )}
+          </div>
+
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("password", userPassword)}
+            />
+            {errors?.password && (
+              <div>
+                <span className="text-red-500">{errors.password.message}</span>
+              </div>
+            )}
+          </div>
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full bg-black text-white px-4 py-2 rounded-md hover:bg-gray-700 duration-200"
+            >
+              Sign-In
+            </button>
+          </div>
+          <p className="mt-8 text-sm text-center text-gray-700">
+            Don&apos;t have an account?{" "}
+            <a href="/register" className="font-medium hover:underline ">
+              Sign-Up
+            </a>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 }
--- frontend/src/store/thunkFunctions.js
+++ frontend/src/store/thunkFunctions.js
@@ -6,7 +6,19 @@ export const registerUser = createAsyncThunk(
   async (body, thunkAPI) => {
     try {
       const response = await axiosInstance.post("/users/register", body);
-      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error.response.data || error.message);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "user/loginUser",
+  async (body, thunkAPI) => {
+    try {
+      const response = await axiosInstance.post("/users/login", body);
       return response.data;
     } catch (error) {
       console.log(error);
