5252e71780b4555868ab386662005382cf65d4f1
Ina Yoon <yia0319@gmail.com>
2024-01-04 21:13:45 +0900

    Create login route and function

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/register", async (req, res, next) => {
@@ -7,6 +8,34 @@ router.post("/register", async (req, res, next) => {
     const user = new User(req.body);
     await user.save();
     return res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    //check if the user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).send("Auth failed, email not found");
+    }
+
+    //check if the password is correct
+    const isMatch = await user.comparePassword(req.body.password);
+    if (!isMatch) {
+      return res.status(400).send("Wrong credentials");
+    }
+
+    const payload = {
+      userId: user._id.toHexString(),
+    };
+
+    //generate the token
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return res.json({ user, accessToken });
   } catch (error) {
     next(error);
   }
